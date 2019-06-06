405d0afd06a5c172d3bef5e98a18f72a911a57c8
kdmrx <harris.eric5@gmail.com>
2019-06-06 14:56:33 -0700

    Extremely basic into to tetris application

--- Applications/Tetris/tetris.py
+++ Applications/Tetris/tetris.py
@@ -1,33 +1,64 @@
 import sys, pygame
 
+
+class SingleBlock(pygame.sprite.Sprite):
+    def __init__(self, color, width):
+        super().__init__()
+        self.color = color
+
+        self.image = pygame.Surface([width, width])
+        self.image.fill(color)
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rect = self.image.get_rect()
+
+
+class Tetris(object):
+    def __init__(self):
+        super().__init__()
+
+        pygame.init()
+
+        self.board_size = 500, 900
+        self.screen = pygame.display.set_mode(self.board_size)
+        self.surface = pygame.Surface(self.board_size)
+
+        # TODO: Test code below:
+
+        self.block = SingleBlock((255, 9, 9), 40)
+
+        # TODO: End Test Code
+
+    def mainloop(self):
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.surface, self.block)
+            pygame.display.flip()
+
+
 if __name__ == '__main__':
 
-    pygame.init()
+    """
+    So what's the breakdown here? 
+    I think dividing everything into single blocks makes the most sense. That way any tetris block is just an
+    arrangement of 4 blocks that can be individually deleted. But how do you shift them down once they have been
+    deleted? 
+    
+    I think the sprites and groups will solve this for us. We can define a base sprite as a single block, then place
+    those into groups for each type of block.
+        When that block ends its downward trip, shift the ownership of the sprits to horizontal line groups. We can
+        simply iterate through all the line groups, triggering deletions if the group contains more than (8? 10?)
+        blocks. This triggers simple delete and shift operations for the affected groups. 
+    Bonus: Adding and removing sprites from groups is a very fast operation.
+        
+    Sprite and Group documentation will be great here.
+    """
 
-    size = width, height = 700, 240
-    speed = [2, 2]
-    black = 0, 0, 0
-
-    screen = pygame.display.set_mode(size)
-
-    surface = pygame.Surface(size)
-
-    ball = pygame.image.load("beach-ball.png")
-    ballrect = ball.get_rect()
-
-    long_bar = pygame.draw.polygon(surface, pygame.Color(255, 0, 0, 0), [(0, 0), (30, 0), (30, 120), (0, 120)])
-    # long_bar_rect = long_bar.get_rect()
-
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
-
-        screen.fill(black)
-        screen.blit(surface, long_bar)
-        pygame.display.flip()
+    game = Tetris()
+    game.mainloop()
