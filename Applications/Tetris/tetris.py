10691a57eb07286d09fd61c0fc4258f8336ff810
erC <harris.eric5@gmail.com>
2019-06-08 14:42:54 -0700

    Shift blocks above completed lines down. Add in a score calculation

--- Applications/Tetris/tetris.py
+++ Applications/Tetris/tetris.py
@@ -129,5 +129,6 @@ class Tetris(object):
 
 if __name__ == '__main__':
 
-    game = Tetris([1, 1, 1, 1, 1, 1, 1, 1, 0, 0])
+    # game = Tetris([1, 1, 1, 1, 1, 1, 1, 1, 0, 0])
+    game = Tetris()
     game.mainloop()
