95adf393ce7fcccf9ab83995fefbffbfc52ea8b8
erC <harris.eric5@gmail.com>
2019-06-23 07:25:19 -0700

    Slightly improve the multiprocessing flow

--- Applications/Tetris/NEATtetris.py
+++ Applications/Tetris/NEATtetris.py
@@ -4,23 +4,47 @@ from Applications.Tetris.tetris import Tetris
 from Source.main import NEATSession
 from Source.constants import get_config
 
+import gc
+
 from multiprocessing import Process, Pipe
 
 
-def play_game(child_conn, _standardized_block_queue):
-    population = child_conn.recv()
+class ParallelTetrisPlayer(object):
+    def __init__(self, _child_conn):
+        super().__init__()
+        self.proc = Process(target=self.play_game)
+        self.child_conn = _child_conn
 
-    for individual in population:
-        tetris_game = Tetris(block_queue=_standardized_block_queue[:])
-        game_over, board_state = tetris_game.do_frame(None)
+    def start(self):
+        self.proc.start()
 
-        while not game_over:
-            output_vector = individual.frame(board_state)
-            strongest_signal = output_vector.index(max(output_vector))
-            game_over, board_state = tetris_game.do_frame(strongest_signal)
-        individual.fitness = tetris_game.score
+    def play_game(self):
+        while True:
+            sub_population = None
+            block_queue = None
+            tetris_game = None
+            game_over = None
+            board_state = None
 
-    child_conn.send(population)
+            time.sleep(3)
+
+            sub_population, block_queue = self.child_conn.recv()
+            if sub_population == "stop":
+                break
+
+            for individual in sub_population:
+                tetris_game = Tetris(block_queue=block_queue[:])
+                game_over, board_state = tetris_game.do_frame(None)
+
+                while not game_over:
+                    output_vector = individual.frame(board_state)
+                    strongest_signal = output_vector.index(max(output_vector))
+                    game_over, board_state = tetris_game.do_frame(strongest_signal)
+                individual.fitness = tetris_game.score
+
+            self.child_conn.send(sub_population)
+
+        print("Ending Process")
 
 
 if __name__ == '__main__':
@@ -28,7 +52,26 @@ if __name__ == '__main__':
     xor_config = get_config("tetris_config")
     session = NEATSession(200, 5, xor_config)
 
-    for generation in range(800):
+    parent_conn, child_conn = Pipe()
+    parent_conn2, child_conn2 = Pipe()
+    parent_conn3, child_conn3 = Pipe()
+    parent_conn4, child_conn4 = Pipe()
+
+    p = ParallelTetrisPlayer(child_conn)
+    p2 = ParallelTetrisPlayer(child_conn2)
+    p3 = ParallelTetrisPlayer(child_conn3)
+    p4 = ParallelTetrisPlayer(child_conn4)
+
+    p.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    for generation in range(200):
+        population, population_1, population_2, population_3, population_4 = None, None, None, None, None
+
+        gc.collect()
+        time.sleep(1)
 
         standardized_block_queue = [1] * 10 + [0, 0] + [random.choice(range(5)) for i in range(100)]
 
@@ -36,50 +79,30 @@ if __name__ == '__main__':
         population = session.get_individuals()
 
         step = int(len(population) / 4)  # Split the population into thirds and run the evaluations in parallel
-        first = population[:step]
-        second = population[step: 2 * step]
-        third = population[2 * step:3 * step]
-        fourth = population[3 * step:]
-        split_population = [first, second, third, fourth]
 
-        parent_conn, child_conn = Pipe()
-        parent_conn2, child_conn2 = Pipe()
-        parent_conn3, child_conn3 = Pipe()
-        parent_conn4, child_conn4 = Pipe()
-
-        p = Process(target=play_game, args=(child_conn, standardized_block_queue[:]))
-        p2 = Process(target=play_game, args=(child_conn2, standardized_block_queue[:]))
-        p3 = Process(target=play_game, args=(child_conn3, standardized_block_queue[:]))
-        p4 = Process(target=play_game, args=(child_conn4, standardized_block_queue[:]))
-
-        p.start()
-        p2.start()
-        p3.start()
-        p4.start()
-
-        parent_conn.send(first)
-        parent_conn2.send(second)
-        parent_conn3.send(third)
-        parent_conn4.send(fourth)
+        parent_conn.send((population[:step], standardized_block_queue))
+        parent_conn2.send((population[step: 2 * step], standardized_block_queue))
+        parent_conn3.send((population[2 * step:3 * step], standardized_block_queue))
+        parent_conn4.send((population[3 * step:], standardized_block_queue))
 
         population_1 = parent_conn.recv()
         population_2 = parent_conn2.recv()
         population_3 = parent_conn3.recv()
         population_4 = parent_conn4.recv()
 
-        p.join()
-        p2.join()
-        p3.join()
-        p4.join()
-
         population = population_1 + population_2 + population_3 + population_4
 
-        print("Generation: {0}".format(generation), time.time() - start)
+        av_fit = sum([individual.fitness for individual in population])/len(population)
+
+        print("Gen: {0}, average fitness: {1}. Generation Time: {2}".format(generation, av_fit, time.time() - start))
 
         session.collect_individuals(population)
         session.advance_generation()
 
-        time.sleep(5)
+    parent_conn.send(("stop", []))
+    parent_conn2.send(("stop", []))
+    parent_conn3.send(("stop", []))
+    parent_conn4.send(("stop", []))
 
     session.show_stats()
 
